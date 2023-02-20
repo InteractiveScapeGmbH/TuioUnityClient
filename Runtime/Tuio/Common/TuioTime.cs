d8cbd909b8d9cf022cc585d573c8760a3bea2c9d
Erich Querner <querner@interactive-scape.com>
2023-02-20 10:38:50 +0100

    add operators for equality

--- Runtime/Tuio/Common/TuioTime.cs
+++ Runtime/Tuio/Common/TuioTime.cs
@@ -5,9 +5,19 @@ namespace Tuio.Common
 {
     public class TuioTime
     {
+        /// <summary>
+        /// The start time of the session.
+        /// </summary>
         private static TuioTime _startTime;
 
+        /// <summary>
+        /// The time since session started in seconds.
+        /// </summary>
         private readonly long _seconds;
+        
+        /// <summary>
+        /// Time fraction in microseconds.
+        /// </summary>
         private readonly long _microseconds;
 
         public TuioTime(long seconds, long microseconds)
@@ -21,6 +31,12 @@ namespace Tuio.Common
             return new TuioTime(oscTimeTag.SecondsSinceEpoch, oscTimeTag.FractionalSecond);
         }
 
+        /// <summary>
+        /// Sums the provided time value represented in total microseconds to the base TuioTime.
+        /// </summary>
+        /// <param name="time">The base TuioTime.</param>
+        /// <param name="microseconds">The total time to add in microseconds.</param>
+        /// <returns>The sum of this TuioTime with the provided time in microseconds.</returns>
         public static TuioTime operator +(TuioTime time, long microseconds)
         {
             long sec = time._seconds + microseconds / 1000000;
@@ -28,6 +44,12 @@ namespace Tuio.Common
             return new TuioTime(sec, microsec);
         }
 
+        /// <summary>
+        /// Sums the provided TuioTime to the base TuioTime.
+        /// </summary>
+        /// <param name="timeA">The base TuioTime.</param>
+        /// <param name="timeB">The TuioTime to add.</param>
+        /// <returns>Sum of this TuioTime with the provided TuioTime.</returns>
         public static TuioTime operator +(TuioTime timeA, TuioTime timeB)
         {
             long sec = timeA._seconds + timeB._seconds;
@@ -36,7 +58,13 @@ namespace Tuio.Common
             microsec %= 1000000;
             return new TuioTime(sec, microsec);
         }
-
+        
+        /// <summary>
+        /// Subtracts the provided time represented in microseconds from the base TuioTime.
+        /// </summary>
+        /// <param name="time">The base TuioTime.</param>
+        /// <param name="microseconds">The total time to subtract in microseconds.</param>
+        /// <returns>The subtraction result of this TuioTime minus the provided time in microseconds.</returns>
         public static TuioTime operator -(TuioTime time, long microseconds)
         {
             long sec = time._seconds - microseconds / 1000000;
@@ -50,6 +78,12 @@ namespace Tuio.Common
             return new TuioTime(sec, microsec);
         }
 
+        /// <summary>
+        /// Subtracts the provided TuioTime from the base TuioTime.
+        /// </summary>
+        /// <param name="timeA">The base TuioTime.</param>
+        /// <param name="timeB">The TuioTime to subtract.</param>
+        /// <returns>The subtraction result of this TuioTime minus the provided TuioTime.</returns>
         public static TuioTime operator -(TuioTime timeA, TuioTime timeB)
         {
             long sec = timeA._seconds - timeB._seconds;
@@ -61,6 +95,38 @@ namespace Tuio.Common
             }
 
             return new TuioTime(sec, microsec);
+        }
+        
+        /// <summary>
+        /// Check for equality of this TuioTime and a given one.
+        /// </summary>
+        /// <param name="time">The TuioTime to compare</param>
+        /// <returns>True if the TuioTime are equal in seconds and microseconds.</returns>
+        public bool Equals(TuioTime time)
+        {
+            return (_seconds == time._seconds) && (_microseconds == time._microseconds);
+        }
+        
+        /// <summary>
+        /// Compare two TuioTimes for equality.
+        /// </summary>
+        /// <param name="timeA">The first TuioTime.</param>
+        /// <param name="timeB">The second TuioTime.</param>
+        /// <returns>True if both TuioTimes are equal in seconds and microseconds.</returns>
+        public static bool operator ==(TuioTime timeA, TuioTime timeB)
+        {
+            return timeA.Equals(timeB);
+        }
+    
+        /// <summary>
+        /// Compare two TuioTimes for inequality.
+        /// </summary>
+        /// <param name="timeA">The first TuioTime.</param>
+        /// <param name="timeB">The second TuioTime.</param>
+        /// <returns>True if both TuioTimes are unequal in seconds or microseconds.</returns>
+        public static bool operator !=(TuioTime timeA, TuioTime timeB)
+        {
+            return !(timeA == timeB);
         }
 
         public TuioTime Subtract(TuioTime other)
