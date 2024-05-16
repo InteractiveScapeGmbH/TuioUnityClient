cd4422fcc1dd1509bd26d336bdabd242ad93c271
Erich Querner <querner@interactive-scape.com>
2024-05-16 15:53:03 +0200

    refactor editor script to match runtime changes

--- Editor/TuioEditor.cs
+++ Editor/TuioEditor.cs
@@ -7,21 +7,28 @@ namespace TuioUnity
 {
     public class TuioEditor
     {
-        [MenuItem("GameObject/TUIO/TUIO 1.1 Manager", false, 1)]
+        [MenuItem("GameObject/TUIO/TUIO 1.1 Session", false, 1)]
         static void CreateTuio11Manager(MenuCommand menuCommand)
         {
-            GameObject go = new GameObject("TUIO 1.1 Manager");
-            go.AddComponent<TuioManagerBehaviour>();
-            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
-            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
-            Selection.activeObject = go;
+            CreateSession(menuCommand, TuioVersion.Tuio11);
         }
         
-        [MenuItem("GameObject/TUIO/TUIO 2.0 Manager", false, 2)]
+        [MenuItem("GameObject/TUIO/TUIO 2.0 Session", false, 2)]
         static void CreateTuio20Manager(MenuCommand menuCommand)
         {
-            GameObject go = new GameObject("TUIO 2.0 Manager");
-            go.AddComponent<TuioManagerBehaviour>();
+            CreateSession(menuCommand, TuioVersion.Tuio20);
+        }
+
+        private static void CreateSession(MenuCommand menuCommand, TuioVersion version)
+        {
+            var name = version switch
+            {
+                TuioVersion.Tuio11 => "TUIO 1.1 Session",
+                TuioVersion.Tuio20 => "TUIO 2.0 Session"
+            };
+            GameObject go = new GameObject(name);
+            var session = go.AddComponent<TuioSession>();
+            session.TuioVersion = version;
             GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
             Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
             Selection.activeObject = go;
