1c6a10ee51cae3c27188d6fa230833f56cfb2aa3
Erich Querner <querner@interactive-scape.com>
2024-05-16 14:48:41 +0200

    add enum for tuio version

---
+++ Runtime/Common/TuioType.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace TuioUnity.Common
+{
+    public enum TuioType
+    {
+        [InspectorName("Tuio 1.1")]
+        Tuio11,
+        [InspectorName("Tuio 2.0")]
+        Tuio20
+    }
+}
