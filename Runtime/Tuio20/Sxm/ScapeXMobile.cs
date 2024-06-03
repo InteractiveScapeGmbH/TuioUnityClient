d56190d2412c2f8ec927d64c76708cf1f256e2f4
Erich Querner <querner@interactive-scape.com>
2024-06-03 15:02:49 +0200

    create unity component to get scape x mobile config from the interactive scape touch table

---
+++ Runtime/Tuio20/Sxm/ScapeXMobile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OSC.NET;
+using TuioNet.Common;
+using TuioUnity.Common;
+using UnityEngine;
+
+namespace TuioUnity.Tuio20.Sxm
+{
+    [RequireComponent(typeof(TuioSession))]
+    public class ScapeXMobile : MonoBehaviour
+    {
+        public SxmConfig Config { get; private set; }
+
+        public event Action<SxmConfig> OnConfigUpdate;
+        
+        private const string ScapeXMobileProfile = "/scape_x_mobile/def";
+
+        private void Start()
+        {
+            var session = GetComponent<TuioSession>();
+            var listener = new MessageListener(ScapeXMobileProfile, OnMessage);
+            session.AddMessageListener(listener);
+        }
+
+        private void OnMessage(OSCMessage message)
+        {
+            var roomId = (string)message.Values[1];
+            var mqttUrl = (string)message.Values[2];
+
+            if (roomId == Config.RoomId && mqttUrl == Config.MqttUrl) return;
+            Config = new SxmConfig(roomId, mqttUrl);
+            OnConfigUpdate?.Invoke(Config);
+        }
+    }
+}
