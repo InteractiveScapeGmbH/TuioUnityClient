e85e5d202f74084abb990a087f15e8985026119d
Erich Querner <querner@interactive-scape.com>
2024-06-03 14:58:55 +0200

    add struct which holds configuration for scape x mobile

---
+++ Runtime/Tuio20/Sxm/SxmConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace TuioUnity.Tuio20.Sxm
+{
+    public struct SxmConfig
+    {
+        public string RoomId { get; }
+        public string MqttUrl { get; }
+
+        public SxmConfig(string roomId, string mqttUrl)
+        {
+            RoomId = roomId;
+            MqttUrl = mqttUrl;
+        }
+    }
+}
