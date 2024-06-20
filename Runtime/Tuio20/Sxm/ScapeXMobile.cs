c9ae296f4f573d090c985e50fc64e72f9ea8c982
Erich Querner <querner@interactive-scape.com>
2024-06-20 16:06:08 +0200

    create unity component which starts bridge

---
+++ Runtime/Tuio20/Sxm/ScapeXMobile.cs
@@ -0,0 +1,25 @@
+﻿using SxmMqttBridge;
+using TuioUnity.Common;
+using UnityEngine;
+
+namespace TuioUnity.Tuio20.Sxm
+{
+    [RequireComponent(typeof(TuioSessionBehaviour))]
+    public class ScapeXMobile : MonoBehaviour
+    {
+        private MqttBridge _bridge;
+        private TuioSessionBehaviour _session;
+
+        private void Start()
+        {
+            _session = GetComponent<TuioSessionBehaviour>();
+            _bridge = new MqttBridge(_session.IpAddress);
+        }
+
+        private void OnApplicationQuit()
+        {
+            if(isActiveAndEnabled)
+                _bridge.Dispose();
+        }
+    }
+}
